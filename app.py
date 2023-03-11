cff56daf44b22cd578d2a68f5b49381ad839ba29
Ahmed El-Sayed <amelsayed@citglobal.com.eg>
2023-03-11 23:52:13 +0200

    change code

--- app.py
+++ app.py
@@ -11,22 +11,21 @@ email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 def app():
     st.title('CV Keyword Rating App')
     st.text('Add your Categories, Keywords entire each Category & Score')
-
     # Get user input for categories, keywords, and scores
     categories = {}
-    num_categories = st.number_input('Number of categories', min_value=1, max_value=10, value=5)
+    num_categories = st.number_input('Number of categories', min_value=1, max_value=10, value=5, key='categories')
     for i in range(num_categories):
-        category_name = st.text_input(f'Category {i+1} name')
-        num_keywords = st.number_input(f'Number of keywords for {category_name}', min_value=1, max_value=20, value=3)
+        category_name = st.text_input(f'Category {i+1} name', key=f'category_name_{i}')
+        num_keywords = st.number_input(f'Number of keywords for {category_name}', min_value=1, max_value=20, value=3, key=f'num_keywords_{i}')
         keywords = {}
         for j in range(num_keywords):
-            keyword_name = st.text_input(f'Keyword {j+1} for {category_name}')
-            keyword_score = st.number_input(f'Score for {keyword_name}', min_value=1, max_value=10, value=3)
+            keyword_name = st.text_input(f'Keyword {j+1} for {category_name}', key=f'keyword_name_{i}_{j}')
+            keyword_score = st.number_input(f'Score for {keyword_name}', min_value=1, max_value=10, value=3, key=f'keyword_score_{i}_{j}')
             keywords[keyword_name.lower()] = keyword_score
         categories[category_name.lower()] = keywords
 
     # Create a file uploader for PDF files
-    uploaded_files = st.file_uploader('Upload one or more CVs in PDF format', type='pdf', accept_multiple_files=True)
+    uploaded_files = st.file_uploader('Upload one or more CVs in PDF format', type='pdf', accept_multiple_files=True, key='uploaded_files')
 
     if uploaded_files is not None:
         # Create a list to store the results
